dde5c16e29e4a378056b24189fe71a56b9356ee6
NafisAshraf <nafis.ashraf.roudra@g.bracu.ac.bd>
2023-09-25 18:48:23 +0600

    Changed forgot pass ui

--- components/ForgotPass.jsx
+++ components/ForgotPass.jsx
@@ -24,45 +24,48 @@ export default function ForgotPass() {
     };
 
     return (
-        <div className="flex justify-center items-center h-screen">
-            {isEmailSent ? (
-                <div className="text-center">
-                    <p className="text-2xl font-bold text-primary mb-4">Password Reset Email Sent</p>
-                    <p className="text-gray-600 mb-6">
-                        Please check your email for instructions to reset your password.
-                    </p>
-                </div>
-            ) : (
-                <form
-                    onSubmit={handleEmailSubmit}
-                    className="w-full max-w-md bg-white p-6 rounded-lg shadow-lg"
-                >
-                    <h1 className="text-2xl font-bold text-center text-primary mb-4">Password Reset</h1>
-                    <p className="text-gray-600 text-center mb-6">
-                        Enter the email address that you logged in with, and we'll send you an email with instructions to reset your password.
-                    </p>
-                    <div className="mb-4">
-                        <label htmlFor="typeEmail" className="block text-gray-700 text-sm font-bold mb-2">Email Address</label>
-                        <input
-                            type="email"
-                            id="email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            className="form-input w-full border rounded-lg py-2 px-3 text-gray-700 focus:outline-none focus:ring focus:border-primary"
-                            placeholder="Enter your email"
-                        />
+        <div className=" bg-gray-100 h-full">
+
+            <div className="flex justify-center items-center h-screen">
+                {isEmailSent ? (
+                    <div className="text-center">
+                        <p className="text-2xl font-bold text-primary mb-4">Password Reset Email Sent</p>
+                        <p className="text-gray-600 mb-6">
+                            Please check your email for instructions to reset your password.
+                        </p>
                     </div>
-                    {error && (
-                        <p className="text-red-500 text-sm mb-4">{error}</p>
-                    )}
-                    <button
-                        type="submit"
-                        className="bg-primary text-white py-2 px-4 rounded-lg w-full hover:bg-primary-dark focus:outline-none focus:ring focus:bg-primary-dark"
+                ) : (
+                    <form
+                        onSubmit={handleEmailSubmit}
+                        className="w-full max-w-md bg-white p-6 rounded-lg shadow-lg"
                     >
-                        Reset Password
-                    </button>
-                </form>
-            )}
+                        <h1 className="text-2xl font-bold text-center text-primary mb-4">Password Reset</h1>
+                        <p className="text-gray-600 text-center mb-6">
+                            Enter the email address that you logged in with, and we'll send you an email with instructions to reset your password.
+                        </p>
+                        <div className="mb-4">
+                            <label htmlFor="typeEmail" className="block text-gray-700 text-sm font-bold mb-2">Email Address</label>
+                            <input
+                                type="email"
+                                id="email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                className="form-input w-full border rounded-lg py-2 px-3 text-gray-700 focus:outline-none focus:ring focus:border-primary"
+                                placeholder="Enter your email"
+                            />
+                        </div>
+                        {error && (
+                            <p className="text-red-500 text-sm mb-4">{error}</p>
+                        )}
+                        <button
+                            type="submit"
+                            className="bg-primary text-white py-2 px-4 rounded-lg w-full hover:bg-primary-dark focus:outline-none focus:ring focus:bg-primary-dark"
+                        >
+                            Reset Password
+                        </button>
+                    </form>
+                )}
+            </div>
         </div>
     );
 }
